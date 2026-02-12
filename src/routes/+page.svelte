47b60489a10616047b73cec6cd848d99249ae569
Young Seo Kim <youngseokim7@gmail.com>
2026-02-12 16:23:39 +0900

    format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,97 +1,95 @@
 <script lang="ts">
+  import defaultWrongAnswerGif from "$lib/assets/among-us-twerk.gif";
+  import defaultCorrectAnswerGif from "$lib/assets/amongus-dance.gif";
 
-  import defaultWrongAnswerGif from '$lib/assets/among-us-twerk.gif';
-  import defaultCorrectAnswerGif from '$lib/assets/amongus-dance.gif'
+  import Component1 from "$lib/components/Component1.svelte";
+  import Component2 from "$lib/components/Component2.svelte";
+  import Component3 from "$lib/components/Component3.svelte";
+  import Component4 from "$lib/components/Component4.svelte";
 
-	import Component1 from '$lib/components/Component1.svelte';
-	import Component2 from '$lib/components/Component2.svelte';
-	import Component3 from '$lib/components/Component3.svelte';
-	import Component4 from '$lib/components/Component4.svelte';
+  let currentStage = 0;
+  let wrongAnswerGif = "";
+  let correctAnswerGif = "";
 
-	let currentStage = 0;
-	let wrongAnswerGif = '';
-  let correctAnswerGif = '';
+  const components = [Component1, Component2, Component3, Component4];
 
-	const components = [
-		Component1,
-		Component2,
-		Component3,
-		Component4
-	];
+  function nextStage() {
+    if (currentStage < 3) {
+      currentStage++;
+    }
+  }
 
-	function nextStage() {
-		if (currentStage < 3) {
-			currentStage++;
-		}
-	}
-
-	function triggerWrongAnswer(gifPath: string = defaultWrongAnswerGif) {
-		wrongAnswerGif = gifPath;
-		setTimeout(() => {
-			wrongAnswerGif = '';
-		}, 2000);
-	}
+  function triggerWrongAnswer(gifPath: string = defaultWrongAnswerGif) {
+    wrongAnswerGif = gifPath;
+    setTimeout(() => {
+      wrongAnswerGif = "";
+    }, 2000);
+  }
 
   function triggerCorrectAnswer(gifPath: string = defaultCorrectAnswerGif) {
     correctAnswerGif = gifPath;
     setTimeout(() => {
-      correctAnswerGif = '';
-    }, 2000)
+      correctAnswerGif = "";
+    }, 2000);
   }
-
 </script>
 
 <main>
-	{#if wrongAnswerGif}
-		<div class="explosion-overlay">
-			<img src={wrongAnswerGif} alt="Wrong Answer" />
-		</div>
-	{/if}
-	{#if correctAnswerGif}
-		<div class="explosion-overlay">
-			<img src={correctAnswerGif} alt="Correct Answer" />
-		</div>
-	{/if}
-	{#if components[currentStage]}
-		<svelte:component this={components[currentStage]} {nextStage} {triggerWrongAnswer} {triggerCorrectAnswer} />
-	{/if}
+  {#if wrongAnswerGif}
+    <div class="explosion-overlay">
+      <img src={wrongAnswerGif} alt="Wrong Answer" />
+    </div>
+  {/if}
+  {#if correctAnswerGif}
+    <div class="explosion-overlay">
+      <img src={correctAnswerGif} alt="Correct Answer" />
+    </div>
+  {/if}
+  {#if components[currentStage]}
+    <svelte:component
+      this={components[currentStage]}
+      {nextStage}
+      {triggerWrongAnswer}
+      {triggerCorrectAnswer}
+    />
+  {/if}
 </main>
 
-	<style>
-	:global(body, html) {
-		margin: 0;
-		padding: 0;
-		width: 100%;
-		height: 100%;
-		overflow: hidden;
-	}
-	
-	main {
-		margin: 0;
-		padding: 0;
-		font-family: Arial, sans-serif;
-		width: 100vw;
-		height: 100vh;
-		display: block;
-		position: relative;
-	}
-	
-	.explosion-overlay {
-		position: fixed;
-		top: 0;
-		left: 0;
-		width: 100vw;
-		height: 100vh;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		background: rgba(0, 0, 0, 0.8);
-		z-index: 1000;
-	}
-	
-	.explosion-overlay img {
-		max-width: 80%;
-		max-height: 80%;
-		object-fit: contain;
-	}
+<style>
+  :global(body, html) {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+
+  main {
+    margin: 0;
+    padding: 0;
+    font-family: Arial, sans-serif;
+    width: 100vw;
+    height: 100vh;
+    display: block;
+    position: relative;
+  }
+
+  .explosion-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: rgba(0, 0, 0, 0.8);
+    z-index: 1000;
+  }
+
+  .explosion-overlay img {
+    max-width: 80%;
+    max-height: 80%;
+    object-fit: contain;
+  }
 </style>
